4024c0d909919e00e7b2192f0f9e10f590d8e982
Page <rickpagersc@gmail.com>
2018-05-07 19:56:20 -0400

    channels

---
+++ project/chat/urls.py
@@ -0,0 +1,9 @@
+# chat/urls.py
+from django.urls import path, re_path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    re_path(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+]
